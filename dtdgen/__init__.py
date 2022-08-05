f67fc5a8d5a62a01bf2863e047e154a4424626cb
Phil Hanna <ph1204@gmail.com>
2022-08-05 19:55:04 -0400

    Added ElementModel to __init__

--- dtdgen/__init__.py
+++ dtdgen/__init__.py
@@ -1,9 +1,11 @@
 from .attribute_model import AttributeModel
 from .child_model import ChildModel
 from .element_model import ElementModel
+from .document_model import DocumentModel
 
 __all__ = [
     'AttributeModel',
     'ChildModel',
     'ElementModel',
+    'DocumentModel',
 ]
