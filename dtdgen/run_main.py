36537c6ce1437a25ed6aae325b240890728e8773
Phil Hanna <ph1204@gmail.com>
2022-08-06 13:07:22 -0400

    Started on RunMain class

---
+++ dtdgen/run_main.py
@@ -0,0 +1,9 @@
+from typing import List, Optional
+
+
+class RunMain:
+    """ Runs DTDGEN """
+    def __init__(self):
+        self.input_files: List[str] = []
+        self.output_file: Optional[str] = None
+        pass
