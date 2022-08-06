056cee7194c5af4a8693532e2191494de91c6ce1
Phil Hanna <ph1204@gmail.com>
2022-08-06 19:32:30 -0400

    Added DTDElementModel stub

--- dtdgen/dtd/__init__.py
+++ dtdgen/dtd/__init__.py
@@ -1,8 +1,10 @@
 """ Classes to generate the DTD for various elements and attributes """
+from .dtd_element_model import DTDElementModel
 from .dtd_element_generator import DTDElementGenerator
 from .dtd_generator import DTDGenerator
 
 __all__ = [
+    'DTDElementModel',
     'DTDElementGenerator',
     'DTDGenerator',
 ]
---
+++ dtdgen/dtd/dtd_element_model.py
@@ -0,0 +1,6 @@
+class DTDElementModel:
+    """
+    Abstract base class for DTD generators for child content,
+    empty elements, mixed content elements, and PCDATA.
+    """
+    pass
