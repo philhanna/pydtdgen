606ee8f84377595bf61fed98491f8fc148395b93
Phil Hanna <ph1204@gmail.com>
2022-08-06 19:24:17 -0400

    Finished DTDElementGenerator

--- dtdgen/dtd/__init__.py
+++ dtdgen/dtd/__init__.py
@@ -1,5 +1,7 @@
+from .dtd_element_generator import DTDElementGenerator
 from .dtd_generator import DTDGenerator
 
 __all__ = [
+    'DTDElementGenerator',
     'DTDGenerator',
 ]
---
+++ dtdgen/dtd/dtd_element_generator.py
@@ -0,0 +1,9 @@
+from dtdgen import ElementModel
+
+
+class DTDElementGenerator:
+    def __init__(self, element_model: ElementModel):
+        pass
+
+    def print_dtd(self, fp):
+        pass
