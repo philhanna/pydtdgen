8147ff0e5f8aea0f87823e64508e8e17346f6bf7
Phil Hanna <ph1204@gmail.com>
2022-08-06 19:56:02 -0400

    Added DTDEmptyElement

--- dtdgen/dtd/dtd_empty_element.py
+++ dtdgen/dtd/dtd_empty_element.py
@@ -1,7 +1,16 @@
+from dtdgen import ElementModel
 from dtdgen.dtd import DTDElementModel
 
 
 class DTDEmptyElement(DTDElementModel):
     """ A DTDElementModel that represents an empty element
     (one that has no children) """
-    pass
+
+    def __init__(self, element_model: ElementModel):
+        """ Creates a new empty element over the specified element model """
+        super().__init__(element_model)
+
+    def __str__(self):
+        """ Generates the <!ELEMENT name EMPTY> string"""
+        output = f"<!ELEMENT {self.element_name} EMPTY>"
+        return output
