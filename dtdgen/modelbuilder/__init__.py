9c937532c71823432c22a2cf3770534a3d9cf1cf
Phil Hanna <ph1204@gmail.com>
2022-08-05 20:37:50 -0400

    Added StackEntry class

---
+++ dtdgen/modelbuilder/__init__.py
@@ -0,0 +1,7 @@
+""" Classes that build a model from an XML instance """
+
+from .stack_entry import StackEntry
+
+__all__ = [
+    'StackEntry',
+]
---
+++ dtdgen/modelbuilder/stack_entry.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from dtdgen import ElementModel
+
+
+class StackEntry:
+    """
+    StackEntry is a data structure that is put on the stack for each nested element
+    """
+    def __init__(self, element_model: ElementModel):
+        self._element_model: ElementModel = element_model
+        self._sequence_number: int = -1
+        self._latest_child_name: Optional[str] = None
+
+    @property
+    def element_model(self):
+        return self._element_model
+
+    @property
+    def sequence_number(self):
+        return self._sequence_number
+
+    def increment_sequence_number(self):
+        self._sequence_number += 1
+
+    @property
+    def latest_child_name(self):
+        return
+
+    @latest_child_name.setter
+    def latest_child_name(self, value: str):
+        self._latest_child_name = value
