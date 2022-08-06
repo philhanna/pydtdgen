f0f0f379c7f8cb74db68d6db571efe26e618d12a
Phil Hanna <ph1204@gmail.com>
2022-08-06 19:45:15 -0400

    Stub for DTDEmptyElement

--- dtdgen/dtd/__init__.py
+++ dtdgen/dtd/__init__.py
@@ -1,10 +1,12 @@
 """ Classes to generate the DTD for various elements and attributes """
 from .dtd_element_model import DTDElementModel
+from .dtd_empty_element import DTDEmptyElement
 from .dtd_element_generator import DTDElementGenerator
 from .dtd_generator import DTDGenerator
 
 __all__ = [
     'DTDElementModel',
+    'DTDEmptyElement',
     'DTDElementGenerator',
     'DTDGenerator',
 ]
---
+++ dtdgen/dtd/dtd_empty_element.py
@@ -0,0 +1,7 @@
+from dtdgen.dtd import DTDElementModel
+
+
+class DTDEmptyElement(DTDElementModel):
+    """ A DTDElementModel that represents an empty element
+    (one that has no children) """
+    pass
