12de391772fd3ecb72e996a266d60c05dbc9ed26
Phil Hanna <ph1204@gmail.com>
2022-08-05 22:26:12 -0400

    Added beginnings of ContentHandler to DocumentModelBuilder

--- dtdgen/modelbuilder/document_model_builder.py
+++ dtdgen/modelbuilder/document_model_builder.py
@@ -1,10 +1,12 @@
+from typing import List
 from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 from xml.sax.xmlreader import XMLReader
 
-from dtdgen import DocumentModel
+from dtdgen import DocumentModel, ElementModel
 
 
-class DocumentModelBuilder:
+class DocumentModelBuilder(ContentHandler):
     """
     Analyzes an instance of an XML document to build a documentModel of
     its structure
@@ -14,8 +16,9 @@ class DocumentModelBuilder:
 
     def __init__(self):
         """ Creates a new DocumentModelBuilder """
-        self._document_model = DocumentModel()
-        self._element_stack = []
+        super().__init__()
+        self.document_model = DocumentModel()
+        self.element_stack: List[ElementModel] = []
 
     def run(self, fp):
         """ Runs an XML input stream through the model builder """
@@ -25,3 +28,21 @@ class DocumentModelBuilder:
             parser.parse(fp)
         finally:
             fp.close()
+
+    def characters(self, content):
+        """ Make a note whether significant character data is found in the element """
+        if len(self.element_stack) > 0:
+            element_model: ElementModel = self.element_stack[0]
+            if not element_model.has_character_content:
+                if not content or not content.strip():
+                    element_model.has_character_content(True)
+
+    def startElement(self, name, attrs):
+        """
+        Handle the start of an element. Record information about the
+        position of this element relative to its parent, and about the
+        attributes of the element.
+        """
+
+        # Create an entry in the Element list, or locate the cached entry
+        pass
