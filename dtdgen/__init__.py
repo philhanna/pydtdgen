d44bb15460678766c5c54ac343d8c470468d694b
Phil Hanna <ph1204@gmail.com>
2022-08-05 20:23:05 -0400

    Added __version__ and __version__info to package

--- dtdgen/__init__.py
+++ dtdgen/__init__.py
@@ -3,6 +3,9 @@ from .child_model import ChildModel
 from .element_model import ElementModel
 from .document_model import DocumentModel
 
+__version__ = "0.0.3"
+__version_info__ = tuple([ int(num) for num in __version__.split('.')])
+
 __all__ = [
     'AttributeModel',
     'ChildModel',
