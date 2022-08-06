73c9e402ad254d12a766908b2ea34300541fb9b6
Phil Hanna <ph1204@gmail.com>
2022-08-05 22:53:15 -0400

    Added DocumentModelBuilder to __init__

--- dtdgen/modelbuilder/__init__.py
+++ dtdgen/modelbuilder/__init__.py
@@ -1,7 +1,9 @@
 """ Classes that build a model from an XML instance """
 
 from .stack_entry import StackEntry
+from .document_model_builder import DocumentModelBuilder
 
 __all__ = [
     'StackEntry',
+    'DocumentModelBuilder',
 ]
