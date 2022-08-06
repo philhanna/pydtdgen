d5c75165c1ea3601de192d023cc94b79e1d5db11
Phil Hanna <ph1204@gmail.com>
2022-08-05 23:04:42 -0400

    Partial completion of DocumentModelBuilder

--- dtdgen/modelbuilder/document_model_builder.py
+++ dtdgen/modelbuilder/document_model_builder.py
@@ -4,6 +4,7 @@ from xml.sax.handler import ContentHandler
 from xml.sax.xmlreader import XMLReader
 
 from dtdgen import DocumentModel, ElementModel
+from dtdgen.modelbuilder import StackEntry
 
 
 class DocumentModelBuilder(ContentHandler):
@@ -18,7 +19,7 @@ class DocumentModelBuilder(ContentHandler):
         """ Creates a new DocumentModelBuilder """
         super().__init__()
         self.document_model = DocumentModel()
-        self.element_stack: List[ElementModel] = []
+        self.element_stack: List[StackEntry] = []
 
     def run(self, fp):
         """ Runs an XML input stream through the model builder """
@@ -32,17 +33,32 @@ class DocumentModelBuilder(ContentHandler):
     def characters(self, content):
         """ Make a note whether significant character data is found in the element """
         if len(self.element_stack) > 0:
-            element_model: ElementModel = self.element_stack[0]
+            element_model: ElementModel = self.element_stack[0].element_model
             if not element_model.has_character_content:
                 if not content or not content.strip():
                     element_model.has_character_content(True)
 
     def startElement(self, name, attrs):
         """
-        Handle the start of an element. Record information about the
+        Handles the start of an element. Records information about the
         position of this element relative to its parent, and about the
         attributes of the element.
         """
 
         # Create an entry in the Element list, or locate the cached entry
-        pass
+        element_model: ElementModel = self.document_model.get_element_model(name)
+        if not element_model:
+            element_model = ElementModel(name)
+            self.document_model.add_element_model(element_model)
+        element_model.increment_occurrences()
+
+        # Retain the associated element details object and initialize
+        # sequence numbering of child element types
+        stack_entry: StackEntry = StackEntry(element_model)
+        self.element_stack.append(stack_entry)
+
+        # Handle the attributes accumulated for this element.
+        # Merge the new attribute list into the existing list for the element.
+        for attrname in attrs.getNames():
+            pass
+
