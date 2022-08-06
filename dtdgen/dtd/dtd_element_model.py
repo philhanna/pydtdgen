2d7725b7098cae22fd0ba506a308691a08428607
Phil Hanna <ph1204@gmail.com>
2022-08-06 19:41:29 -0400

    Finished DTDElementModel

--- dtdgen/dtd/dtd_element_model.py
+++ dtdgen/dtd/dtd_element_model.py
@@ -1,6 +1,17 @@
+from dtdgen import ElementModel
+
+
 class DTDElementModel:
     """
     Abstract base class for DTD generators for child content,
     empty elements, mixed content elements, and PCDATA.
     """
-    pass
+    def __init__(self, element_model: ElementModel):
+        """ Creates a DTDElementModel over the specified element model """
+        self._element_model: ElementModel = element_model
+        self._element_name: str = element_model.name
+        self._n_children: int = element_model.get_child_model_count()
+
+    @property
+    def element_name(self):
+        return self._element_name
