18aaec8fad1c68848ef2e011bf9d647d03ceffb0
Phil Hanna <ph1204@gmail.com>
2022-08-07 15:07:45 -0400

    Added to DTDElementGeneerator

--- dtdgen/dtd/dtd_element_generator.py
+++ dtdgen/dtd/dtd_element_generator.py
@@ -1,9 +1,26 @@
 from dtdgen import ElementModel
+from dtdgen.dtd import DTDElementModel
 
 
 class DTDElementGenerator:
+    """ Assembles the correct DTD statement for an element and outputs it. """
     def __init__(self, element_model: ElementModel):
-        pass
+        self._element_model: ElementModel = element_model
 
     def print_dtd(self, fp):
+        """
+        Constructs the DTD string appropriate for this element and writes
+        it to the specified output.
+        """
+        element_model: ElementModel = self._element_model
+        dtd_element_model: DTDElementModel = None
+
+        # Get the number of children this element can have
+
+        n_children: int = element_model.get_child_model_count()
+        has_character_content: bool = element_model.has_character_content
+
+        # No children - must be either EMPTY or have #PCDATA content
+        # Has children - either element content or mixed content
+
         pass
