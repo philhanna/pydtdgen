f472a49edea3e97b3467cc2d3e9617609dfadd6b
Phil Hanna <ph1204@gmail.com>
2022-08-07 15:15:35 -0400

    Started work on dtd_attribute_list.py

--- dtdgen/dtd/__init__.py
+++ dtdgen/dtd/__init__.py
@@ -2,6 +2,7 @@
 from .dtd_element_model import DTDElementModel
 from .dtd_empty_element import DTDEmptyElement
 from .dtd_element_generator import DTDElementGenerator
+from .dtd_attribute_list import DTDAttributeList
 from .dtd_generator import DTDGenerator
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     'DTDEmptyElement',
     'DTDElementGenerator',
     'DTDGenerator',
+    'DTDAttributeList',
 ]
---
+++ dtdgen/dtd/dtd_attribute_list.py
@@ -0,0 +1,6 @@
+from dtdgen import ElementModel
+
+
+class DTDAttributeList:
+    def __init__(self, element_model: ElementModel):
+        pass
