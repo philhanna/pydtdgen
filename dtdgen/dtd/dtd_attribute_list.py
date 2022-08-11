f114b0410a1a3cecc3f81aec56afc01343dcd118
Phil Hanna <ph1204@gmail.com>
2022-08-11 15:35:47 -0400

    Added __str__ method to DTDAttributeList

--- dtdgen/dtd/dtd_attribute_list.py
+++ dtdgen/dtd/dtd_attribute_list.py
@@ -1,6 +1,15 @@
+from typing import List
+
 from dtdgen import ElementModel
 
 
 class DTDAttributeList:
     def __init__(self, element_model: ElementModel):
-        pass
+        self._element_model: ElementModel = element_model
+        self._alist: List[str] = []
+
+    def __str__(self):
+        """ Construct the list of <!ATTLIST> for an element """
+        element_model = self._element_model
+        element_name = element_model.name
+
