6f0ade58b1e58815b6d78f8be024ef5126c39fcf
Phil Hanna <ph1204@gmail.com>
2022-08-11 16:08:43 -0400

    Added unit test for DTDPCDATAElement

--- tests/test_dtd_pcdata_element.py
+++ tests/test_dtd_pcdata_element.py
@@ -1,5 +1,12 @@
 from unittest import TestCase
 
+from dtdgen import ElementModel
+from dtdgen.dtd import DTDPCDATAElement
+
 
 class TestDTDPCDATAElement(TestCase):
-    pass
+    def test_with_string(self):
+        em = ElementModel("stooges")
+        pcde = DTDPCDATAElement(em)
+        self.assertEqual("<!ELEMENT stooges ( #PCDATA ) >", str(pcde))
+
