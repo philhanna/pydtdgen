8af295f23e467cfb5bb49fb2f2843215bec0157c
Phil Hanna <ph1204@gmail.com>
2022-09-08 16:49:21 -0400

    Removed the name parameter from the constructor

--- dtdgen/dtd_model.py
+++ dtdgen/dtd_model.py
@@ -1,8 +1,4 @@
 class DTDModel:
     """The abstract representation of a DTD"""
-    def __init__(self, name: str):
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
+    def __init__(self):
+        pass
