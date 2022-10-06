3c26b717dfdc44f2b38f000c10e386f22cd21d0f
Phil Hanna <ph1204@gmail.com>
2022-10-05 20:41:52 -0400

    Updated __init__.py with AttributeDetails

--- dtdgen/__init__.py
+++ dtdgen/__init__.py
@@ -1,4 +1,5 @@
 from .child_details import ChildDetails
+from .attribute_details import AttributeDetails
 from .element_details import ElementDetails
 from .stack_entry import StackEntry
 from .functions import escape, is_valid_nmtoken, is_valid_name
@@ -8,6 +9,7 @@ __all__ = [
     'is_valid_nmtoken',
     'is_valid_name',
     'ChildDetails',
+    'AttributeDetails',
     'ElementDetails',
     'StackEntry',
 ]
