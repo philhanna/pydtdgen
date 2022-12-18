f0283523aa8ff4d643465479b30028b1f8ac3ae8
Phil Hanna <ph1204@gmail.com>
2022-12-18 16:56:39 -0500

    Converted test_handle_attributes to pytest

--- tests/test_handle_attributes.py
+++ tests/test_handle_attributes.py
@@ -1,39 +1,37 @@
-from unittest import TestCase
-
 from dtdgen import ElementDetails, SchemaModelBuilder, AttributeDetails
 
 
-class TestHandleAttributes(TestCase):
+def test_rank():
+    # First occurrence of <stooge name="..." rank="...">
+    attrs = {"name": "Larry", "rank": "2"}
+    ed = ElementDetails("stooge")
+    SchemaModelBuilder.handle_attributes(attrs, ed)
+    attributes = ed.attributes
+    assert "rank" in attributes
 
-    def test_rank(self):
-        # First occurrence of <stooge name="..." rank="...">
-        attrs = {"name": "Larry", "rank": "2"}
-        ed = ElementDetails("stooge")
+    attr: AttributeDetails = attributes["rank"]
+    assert "rank" == attr.name
+    assert 1 == attr.occurrences
+    assert attr.unique
+    assert "2" in attr.values
+    assert not attr.all_names  # "2" is not a valid name
+    assert attr.all_nmtokens
+
+
+def test_rank_required():
+    ed = ElementDetails("stooge")
+
+    test_values = [
+        {"name": "Larry", "rank": "2"},
+        {"name": "Curly", "rank": "3"},
+        {"name": "Moe", "rank": "1"},
+    ]
+    for attrs in test_values:
         SchemaModelBuilder.handle_attributes(attrs, ed)
-        attributes = ed.attributes
-        self.assertIn("rank", attributes)
-        attr = attributes["rank"]
-        self.assertEqual("rank", attr.name)
-        self.assertEqual(1, attr.occurrences)
-        self.assertTrue(attr.unique)
-        self.assertIn("2", attr.values)
-        self.assertFalse(attr.all_names)  # "2" is not a valid name
-        self.assertTrue(attr.all_nmtokens)
 
-    def test_rank_required(self):
-        ed = ElementDetails("stooge")
-
-        test_values = [
-            {"name": "Larry", "rank": "2"},
-            {"name": "Curly", "rank": "3"},
-            {"name": "Moe", "rank": "1"},
-        ]
-        for attrs in test_values:
-            SchemaModelBuilder.handle_attributes(attrs, ed)
-
-        # Check what we know about the rank attribute
-        attr: AttributeDetails = ed.attributes["rank"]
-        self.assertEqual("rank", attr.name)
-        self.assertEqual(3, attr.occurrences, "We have seen this attribute 3 times")
-        self.assertTrue(attr.unique, "Every value was different")
-        self.assertSetEqual({"1", "2", "3"}, attr.values)
+    # Check what we know about the rank attribute
+    attr: AttributeDetails = ed.attributes["rank"]
+    assert "rank" == attr.name
+    assert 3 == attr.occurrences
+    assert attr.unique
+    assert {"1", "2", "3"} == attr.values
