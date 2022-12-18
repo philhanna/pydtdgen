723e4c891abfcb5208aceff160bb6bed3b666cc2
Phil Hanna <ph1204@gmail.com>
2022-12-18 16:48:44 -0500

    Converted test_escape to pytest

--- tests/test_escape.py
+++ tests/test_escape.py
@@ -1,5 +1,3 @@
-from unittest import TestCase
-
 from dtdgen.functions import escape
 
 
@@ -7,22 +5,23 @@ def fmt2(n: int):
     return ("0" + str(n))[-2:]
 
 
-class TestEscape(TestCase):
+def test_single_quotes():
+    s = "Now, 'Tommy'\n"
+    expected = "Now, &#39;Tommy&#39;&#10;"
+    actual = escape(s)
+    assert actual == expected
 
-    def test_single_quotes(self):
-        s = "Now, 'Tommy'\n"
-        expected = "Now, &#39;Tommy&#39;&#10;"
-        actual = escape(s)
-        self.assertEqual(expected, actual)
 
-    def test0020(self):
-        for i in range(0x00, 0x20):
-            expected = "&#" + fmt2(i) + ";"
-            actual = escape(chr(i))
-            self.assertEqual(expected, actual)
+def test0020():
+    for i in range(0x00, 0x20):
+        expected = "&#" + fmt2(i) + ";"
+        actual = escape(chr(i))
+        assert actual == expected
 
-    def test80100(self):
-        for i in range(0x80, 0x100):
-            expected = "&#" + fmt2(i) + ";"
-            actual = escape(chr(i))
-            self.assertEqual(expected, actual)
+
+def test80100():
+    for i in range(0x80, 0x100):
+        expected = "&#" + fmt2(i) + ";"
+        actual = escape(chr(i))
+        assert actual == expected
+
